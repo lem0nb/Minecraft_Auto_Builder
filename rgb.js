e0eb360d9d1445ccde28b4146b7986a9cf3bf1fa
lem0nb <68461894+lem0nb@users.noreply.github.com>
2021-07-21 08:12:55 +0800

    Improve RGB color data

---
+++ rgb.js
@@ -0,0 +1,137 @@
+var rgb=[
+[252,252,252,'snow 0'],
+
+[247,247,247,'concrete 0'],
+
+[233,233,233,'quartz_block 0'],
+
+[177,179,177,'clay 0'],
+
+[151,151,151,'concrete 8'],
+
+[102,109,102,'stone 0'],
+
+[58,57,58,'concrete 7'],
+
+[5,5,5,'concrete 15'],
+
+
+
+
+[164,97,33,'concrete 1'],
+//[236,113,16, 'concrete 1'],
+
+[140,78,164,'concrete 2'],
+[80,121,164,'concrete 3'],
+[181,182,41,'concrete 4'],
+[99,158,18,'concrete 5'],
+[189,97,123,'concrete 6'],
+
+//[123,121,123,'concrete 8'],
+
+
+[58,97,115,'concrete 9'],
+[98,50,137,'concrete 10'],
+[36,59,135,'concrete 11'],
+[77,58,39,'concrete 12'],
+[73,95,36,'concrete 13'],
+[119,40,40,'concrete 14'],
+
+
+
+//特殊
+[73,30,110,'mycelium 0'],
+
+
+
+[120,98,68,'planks 1'],
+[115,85,58,'dirt 1'],
+[206,190,66,'gold_block 0'],
+[74,174,173,'diamond_block 0'],
+[58,101,197,'lapis_block 0'],
+[0,174,49,'emerald_block 0'],
+[197,0,0,'redstone_block 0'],
+[0,97,0,'dried_kelp_block 0'],
+[58,101,115,'prismarine 0'],
+[73,174,166,'prismarine 2'],
+[99,142,41,'slime 0'],
+[82,0,0,'nether_brick 0'],
+[115,40,41,'red_nether_brick 0'],
+
+[175,87,185,'purpur_block 0'],
+[82,61,41,'soul_sand 0'],
+[173,101,41,'red_sandstone 0'],
+[197,146,74,'glowstone 0'],
+[107,65,41,'redstone_lamp 0'],
+[74,97,33,'end_portal_frame 0'],
+[123,125,197,'packed_ice 0'],
+[210,210,163,'end_stone 0'],
+[181,178,41,'sponge 0'],
+
+
+
+//羊毛
+[247,248,248,'wool 0'],
+[229,102,4,'wool 1'],
+[186,65,176,'wool 2'],
+[59,181,219,'wool 3'],
+[247,192,33,'wool 4'],
+[109,182,24,'wool 5'],
+[244,146,176,'wool 6'],
+[66,72,75,'wool 7'],
+[143,143,135,'wool 8'],
+[21,139,146,'wool 9'],
+[112,36,164,'wool 10'],
+[44,47,144,'wool 11'],
+[117,73,42,'wool 12'],
+[79,100,31,'wool 13'],
+[143,32,32,'wool 14'],
+[20,20,20,'wool 15'],
+
+
+
+
+
+//陶瓦
+[211,180,162,'stained_hardened_clay 0'],
+[159,82,36,'stained_hardened_clay 1'],
+[149,87,108,'stained_hardened_clay 2'],
+[116,110,139,'stained_hardened_clay 3'],
+[186,133,36,'stained_hardened_clay 4'],
+[107,120,54,'stained_hardened_clay 5'],
+[160,77,78,'stained_hardened_clay 6'],
+[58,43,36,'stained_hardened_clay 7'],
+[133,106,97,'stained_hardened_clay 8'],
+[86,91,91,'stained_hardened_clay 9'],
+[114,66,83,'stained_hardened_clay 10'],
+[76,61,92,'stained_hardened_clay 11'],
+[77,51,36,'stained_hardened_clay 12'],
+[75,83,43,'stained_hardened_clay 13'],
+[139,58,45,'stained_hardened_clay 14'],
+[37,23,18,'stained_hardened_clay 15'],
+
+
+
+//白色 0
+//橙色 1
+//品红色 2
+//淡蓝色 3
+//黄色 4
+//黄绿色 5
+//粉红色 6
+//灰色 7
+//淡灰色 8
+//青色 9
+//紫色 10
+//蓝色 11
+//棕色 12
+//绿色 13
+//黑色 15
+//红色 14
+
+
+
+
+
+];
+module.exports=rgb
